256e61a9890c839a493b4bf632e43f604dec64f0
Iron Tony <lifegamecom1@gmail.com>
2022-01-06 11:42:30 +0200

    log dir

--- train.py
+++ train.py
@@ -25,7 +25,7 @@ parser.add_argument("--patch_size", type=int, default=4, help="Patch size for di
 parser.add_argument("--initial_size", type=int, default=8, help="Initial size for generator.")
 parser.add_argument("--latent_dim", type=int, default=1024, help="Latent dimension of generator's input.")
 parser.add_argument("--diff_aug", type=str, default="color,translation,cutout", help="Data Augmentation")
-parser.add_argument("--log_comment", type=str, default="TransGAN", help="Tensorboard comment to differentiate runs")
+parser.add_argument("--log_dir", type=str, default=None, help="Tensorboard log directory")
 parser.add_argument("--weight_log_iter", type=int, default=100,
                     help="Log weights and gradients every <weight_log_iter> iterations (batches)")
 parser.add_argument("--n_critic", type=int, default=1,
@@ -88,7 +88,7 @@ train_dataset = torchvision.datasets.CIFAR10(root="./data", train=True, download
 ]))
 train_loader = DataLoader(dataset=train_dataset, batch_size=args.batch_size, shuffle=True)
 
-writer = SummaryWriter(log_dir="./runs/", comment=args.log_comment)
+writer = SummaryWriter(log_dir=args.log_dir)
 checkpoint = Checkpoint("./checkpoints/", generator, discriminator, optimizer_gen, optimizer_dis)
 
 iteration = 0
