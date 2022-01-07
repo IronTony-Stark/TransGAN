2d0687387c16c32f0808b474dab4342d0664d1f0
Iron Tony <lifegamecom1@gmail.com>
2022-01-07 18:17:19 +0200

    0.01

--- equalized_lr.py
+++ equalized_lr.py
@@ -2,22 +2,26 @@
 # Introduced in Progressive GAN https://arxiv.org/abs/1710.10196
 # Implementation taken from https://personal-record.onrender.com/post/equalized-lr/
 
+import math
 from math import sqrt
 
+import torch
 from torch import Tensor
 from torch import nn
 from torch.nn import Module
+from torch.nn import functional as F
 
 
 class EqualLR:
-    def __init__(self, name: str):
+    def __init__(self, name: str, multiply: int = 0.01):
         self.name = name
+        self.multiply = multiply
 
     def compute_weight(self, module: Module):
         weight = getattr(module, self.name + '_orig')
         fan_in = weight.data.size(1) * weight.data[0][0].numel()
 
-        return weight * sqrt(2 / fan_in)
+        return weight * sqrt(self.multiply / fan_in)
 
     @staticmethod
     def apply(module: Module, name: str):
@@ -66,3 +70,71 @@ class EqConv2d(Module):
 
     def forward(self, input: Tensor) -> Tensor:
         return self.conv(input)
+
+
+class EqualConv2d(nn.Module):
+    def __init__(
+            self, in_channel, out_channel, kernel_size, stride=1, padding=0, bias=True
+    ):
+        super().__init__()
+
+        self.weight = nn.Parameter(
+            torch.randn(out_channel, in_channel, kernel_size, kernel_size)
+        )
+        self.scale = 1 / math.sqrt(in_channel * kernel_size ** 2)
+
+        self.stride = stride
+        self.padding = padding
+
+        if bias:
+            self.bias = nn.Parameter(torch.zeros(out_channel))
+
+        else:
+            self.bias = None
+
+    def forward(self, input):
+        return F.conv2d(
+            input=input,
+            weight=self.weight * self.scale,
+            bias=self.bias,
+            stride=self.stride,
+            padding=self.padding,
+        )
+
+    def __repr__(self):
+        return (
+            f"{self.__class__.__name__}({self.weight.shape[1]}, {self.weight.shape[0]},"
+            f" {self.weight.shape[2]}, stride={self.stride}, padding={self.padding})"
+        )
+
+
+class EqualLinear(nn.Module):
+    def __init__(
+            self, in_dim, out_dim, bias=True, bias_init=0, lr_mul=1, activation=None
+    ):
+        super().__init__()
+
+        self.weight = nn.Parameter(torch.randn(out_dim, in_dim).div_(lr_mul))
+
+        if bias:
+            self.bias = nn.Parameter(torch.zeros(out_dim).fill_(bias_init))
+
+        else:
+            self.bias = None
+
+        self.activation = activation
+
+        self.scale = (1 / math.sqrt(in_dim)) * lr_mul
+        self.lr_mul = lr_mul
+
+    def forward(self, input):
+        out = F.linear(
+            input, self.weight * self.scale, bias=self.bias * self.lr_mul
+        )
+
+        return out
+
+    def __repr__(self):
+        return (
+            f"{self.__class__.__name__}({self.weight.shape[1]}, {self.weight.shape[0]})"
+        )
