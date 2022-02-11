2f02781f09c3fbf189de3e2991278b7a3c746c19
Iron Tony <lifegamecom1@gmail.com>
2022-02-11 22:50:48 +0200

    comment

--- train.py
+++ train.py
@@ -12,7 +12,7 @@ from utils import *
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--comment", type=str, default="Fake Img Normalization", help="Comment to describe the run.")
+parser.add_argument("--comment", type=str, default="D no augmentation", help="Comment to describe the run.")
 parser.add_argument("--img_size", type=int, default=32, help="Size of image for discriminator input.")
 parser.add_argument("--epoch", type=int, default=200, help="Number of epoch.")
 parser.add_argument("--batch_size", type=int, default=32, help="Batch size.")
